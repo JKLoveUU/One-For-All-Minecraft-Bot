2109c2653375000cb5472b5d4ec84ec762cf3d7f
JKLoveUU <0xzerotwox0@gmail.com>
2023-05-07 01:00:01 +0800

    add discord bots menu

--- generalbot.js
+++ generalbot.js
@@ -2,45 +2,67 @@
 const mineflayer = require("mineflayer");
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 const profiles = require(`${process.cwd()}/profiles.json`);
-if(!profiles[process.argv[2]]){
+if (!profiles[process.argv[2]]) {
     process.exit(2001)
 }
 const bot = (() => { // createMcBot
     const bot = mineflayer.createBot({
-      host: profiles[process.argv[2]].host,
-      port: profiles[process.argv[2]].port,
-      username: profiles[process.argv[2]].username,
-      auth: "microsoft",
-      version: "1.18.2"
+        host: profiles[process.argv[2]].host,
+        port: profiles[process.argv[2]].port,
+        username: profiles[process.argv[2]].username,
+        auth: "microsoft",
+        version: "1.18.2"
     })
-  
-    console.log(`${process.argv[2]} login as ${bot.username}`)
-    bot.on('spawn', async () => {
-      console.log("spawn")
-      //let time = new Date();
+    bot.once('spawn', async () => {
+        console.log(`${process.argv[2]} login as ${bot.username}`)
+        bot.chatAddPattern(
+            /^(\[[A-Za-z0-9-_您]+ -> [A-Za-z0-9-_您]+\] .+)$/,
+            'dm'
+        )
+        bot.chatAddPattern(
+            /^\[系統\] (\S+) 想要傳送到 你 的位置$/,
+            'tpa'
+        )
+        bot.chatAddPattern(
+            /^\[系統\] (\S+) 想要你傳送到 該玩家 的位置$/,
+            'tpahere'
+        )
+        bot.chatAddPattern(
+            /^Summoned to wait by CONSOLE$/,
+            'wait'
+        )
     })
-    bot.on('message',async (jsonMsg) => {
+    bot.on('message', async (jsonMsg) => {
         //console.log(jsonMsg.toString())
         //let time = new Date();
-      })
+    })
+    bot.on('dm', async (jsonMsg) => {
+        console.log(jsonMsg.toString())
+    })
+    bot.on('tpa', p => {
+        bot.chat(true? '/tpaccept' : '/tpdeny')
+    })
+    bot.on('tpahere', p => {
+        bot.chat(true? '/tpaccept' : '/tpdeny')
+    })
     // bot.on('chat', (username, message) => {
     //   if (username === bot.username) return
     //   console.log(message)
     //  // bot.chat(message)
     // })
     bot.on('error', (reason) => {
-        console.log("err "+reason)
+        console.log("err " + reason)
         kill(1000)
     })
     bot.on('kick', (reason) => {
-        console.log("kick reason "+reason)
+        console.log("kick reason " + reason)
         kill(1000)
     })
     bot.on('death', () => {
-      console.log(`Death at ${new Date()}`);
+        console.log(`Death at ${new Date()}`);
     })
     bot.on('end', (reason) => {
-        console.log("end reason "+reason)
+        console.log("end reason " + reason)
         kill(1000)
     })
     //init()
@@ -54,13 +76,13 @@ function kill(code = 1000) {
     process.exit(code)
 }
 process.on('message', (message) => {
-    switch(message.type){
+    switch (message.type) {
         case 'cmd':
             //交給CommandManager
             break;
         case 'chat':
             bot.chat(message.text)
-            console.log(`已傳送訊息至 ${bot.username}: ${message.text}` );
+            console.log(`已傳送訊息至 ${bot.username}: ${message.text}`);
             break;
         case 'reload':
             kill(1002)
--- index.js
+++ index.js
@@ -3,32 +3,37 @@ const readline = require('readline');
 const { fork } = require('child_process');
 const mineflayer = require("mineflayer");
 const fs = require('fs');
-const { testf } = require('./lib/test.js');
+// const { testf } = require('./lib/test.js');
 const toml = require('toml-require').install({ toml: require('toml') });
+const config = require(`${process.cwd()}/config.toml`);
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { Client, Intents, MessageActionRow, MessageButton, MessageOptions, MessagePayload, MessageSelectMenu, MessageEmbed, Message } = require('discord.js');
+const rest = new REST({ version: '9' }).setToken(config.discord_setting.token);
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+let botMenuId = undefined;
+let botMenuLastUpdate = new Date();
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 // const configPath = path.join(__dirname, 'config.toml'); // 使用相对路径访问文件
 // const config = toml.parse(fs.readFileSync(configPath, 'utf8'));
-const config = require(`${process.cwd()}/config.toml`);
 //create logs dir if not exist
 const logsDir = path.join(__dirname, 'logs');
 if (!fs.existsSync(logsDir)) {
-  fs.mkdirSync(logsDir);
+    fs.mkdirSync(logsDir);
 }
-const logFilePath = path.join(logsDir,"lastest"+".log");
+const logFilePath = path.join(logsDir, "lastest" + ".log");
 const logFile = fs.createWriteStream(logFilePath, { flags: 'a' });
 function myLog(...args) {
-  const prefix = '[LOG]';
-  const message = `[${new Date()}] ${prefix} ${args.join(' ')}\n`;
-  logFile.write(message);
+    const prefix = '[LOG]';
+    const message = `[${new Date()}] ${prefix} ${args.join(' ')}\n`;
+    logFile.write(message);
 }
 myLog(`Bot Start at ${new Date().toString()}`);
 const dataManager = {
-    
+
 }
-//let ts = 
-require("./lib/test.js");
-//ts.test()
-testf()
 const bots = {
     name: [],
     bots: [],
@@ -70,7 +75,7 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     completer: (line) => {
-        const completions = ['.switch', '.exit', '.close', '.test','.reload'];
+        const completions = ['.switch', '.exit', '.close', '.test', '.reload'];
         const hits = completions.filter((c) => c.startsWith(line));
         return [hits.length ? hits : completions, line];
     },
@@ -85,6 +90,9 @@ rl.on('line', (input) => {
         const [rlCommandName, ...rlargs] = input.trim().split(/\s+/);
         console.log(`收到指令 ${rlCommandName}`)
         switch (rlCommandName.substring(1)) {
+            case 'eval':    //debug
+                eval(input.substring(6))
+                break;
             case 'list':
                 console.log(`目前共 ${bots.name.length} 隻bot`)
                 for (i in bots.name) {
@@ -130,48 +138,145 @@ rl.on('line', (input) => {
     rl.prompt();
 });
 // 监听 'close' 事件
-rl.on('close', () => {
+rl.on('close', async () => {
     //console.log('退出readLine');
-    handleClose()
-    process.exit(0)
+    await handleClose()
 });
+client.on('ready', async () => {
+    console.log(`Discord bot Logged in as ${client.user.tag}`);
+    client.user.setPresence({
+        activities: [{
+            name: 'In Service',
+            //type: 'Custom',
+        }],
+        status: 'online',
+    });
+    const channel = client.channels.cache.get(config.discord_setting.channelId);
+    //delete all old bot menu
+    const botMenuIds = [];
+    await channel.messages.fetch({ limit: 30 }).then(messages => {
+        const botMessages = messages.filter(m => m.author.id === client.user.id && m.author.bot);
+        const matchingMessages = botMessages.filter(m => {
+            if (m.embeds && m.embeds.length > 0) {
+                const firstEmbed = m.embeds[0];
+                const matchingField = firstEmbed.fields.find(field => field.name.startsWith('目前共'));
+                return (matchingField !== undefined);
+            } else {
+                return false;
+            }
+        });
+        //console.log(matchingMessages)
+        if (matchingMessages) {
+            matchingMessages.forEach(msg => {
+                // console.log(msg)
+                botMenuIds.push(msg.id);
+            });
+        } else {
+        }
+    });
+    channel.bulkDelete(botMenuIds)
+        .then(deletedMessages => console.log(`Deleted ${deletedMessages.size} expired Menu`))
+        .catch(console.error);
+    let newbotMenuId = await channel.send(generateBotMenu());
+    botMenuId = newbotMenuId.id
+    setInterval(async () => {
+        let oldmenu = await getChannelMsgFetch(channel, botMenuId)
+        if (oldmenu) {
+            await oldmenu.edit(generateBotMenu());
+        } else {
+            let newbotMenuId = await channel.send(generateBotMenu());
+            botMenuId = newbotMenuId.id
+        }
+    }, 30_000);
+});
+//botmenu handler 
+client.on('interactionCreate', async (interaction) => {
+    console.log(interaction.customId)
+    if (!interaction.customId.startsWith('botmenu')) {
+        return
+    }
+    switch (interaction.customId) {
+        case 'botmenu-refresh-btn':
+            await interaction.update(generateBotMenu());
+            break;
+        case 'botmenu-shift-btn':
+            const message = await interaction.channel.messages.fetch(interaction.message.id);
+            let newbotMenuId = await interaction.channel.send(generateBotMenu());
+            botMenuId = newbotMenuId.id
+            await message.delete();
+            break;
+        case 'botmenu-close-btn':
+            const closeConfirmButon = interaction.component
+                .setCustomId('botmenu-close-confirm')
+                .setLabel('Click Again To close')
+                .setStyle('DANGER')
+                .setEmoji('⚪');
+            const [row1, row2] = interaction.message.components;
+            await interaction.update({
+                components: [
+                    new MessageActionRow().addComponents(row1.components),
+                    new MessageActionRow().addComponents(row2.components),
+                ],
+            });
 
+            break;
+        case 'botmenu-close-confirm':
+            await interaction.reply({
+                content: 'bot close',
+                ephemeral: true
+            })
+            await handleClose()
+            break;
+        default:
+            await notImplementYet(interaction);
+            break;
+    }
+})
 process.on('uncaughtException', err => {
     console.log('Uncaught:\n', err)
-  })
+})
 process.on('SIGINT', handleClose);
 process.on('SIGTERM', handleClose);
 console.log(`Press Ctrl+C to exit   PID: ${process.pid}`);
 //console.log(config)
+client.login(config.discord_setting.token)
 main()
-function main(){
+function main() {
     console.log(config.account.id)
     currentSelect = 0;
     process.title = 'Test-bot 0 - are in service';
     let tmp = 5;
     //get type  and set of all bot
     // type: auto raid general
-    for(i in config.account){
+    for (i in config.account) {
         //console.log(config.account[i])
     }
-    for(let i = 0; i<config.account.id.length;i++){
+    for (let i = 0; i < config.account.id.length; i++) {
         setTimeout(() => {
-            console.log(i)
-            console.log(config.account.id[i])
+            //console.log(i)
+            //console.log(config.account.id[i])
             createGeneralBot(config.account.id[i]);
-            tmp+=200;
-          }, tmp);
-        
+            tmp += 200;
+        }, tmp);
+
     }
 
 }
 async function handleClose() {
     console.log('Closing application...');
     for (i in bots.name) {
-        if(bots.bots[i].c == undefined) continue
-        bots.bots[i].c.send({ type: "exit"});
+        if (bots.bots[i].c == undefined) continue
+        bots.bots[i].c.send({ type: "exit" });
     }
+    await Promise.all([
+        setBotMenuNotInService(),
+        // new Promise(resolve => setTimeout(resolve, 1000)), // wait for 1 second
+        // wait for all promises to complete
+        //unregisterCommands(client)
+        //anotherAsyncFunction()
+    ]);
     console.log('Close finished');
+    client.destroy();
     process.exit(0);
 }
 function createGeneralBot(name) {
@@ -183,14 +288,134 @@ function createGeneralBot(name) {
     child.on('close', c => {
         child.removeAllListeners()
         bots.setBot(name, undefined)
-        if(c==0) console.log(`${name}: stopped success`)
-        else if(c>=2000){
+        if (c == 0) console.log(`${name}: stopped success`)
+        else if (c >= 2000) {
             console.log(`bot  ${name} err code = ${c}`)
-        }else{
+        } else {
             console.log("bot will restart at 10 second")
-           // bots.setBot(name, setTimeout(() => { createGeneralBot(name) }, 10_000))
-           setTimeout(() => { createGeneralBot(name) }, 10_000)
+            // bots.setBot(name, setTimeout(() => { createGeneralBot(name) }, 10_000))
+            setTimeout(() => { createGeneralBot(name) }, 10_000)
         }
+    })
+}
+async function getChannelMsgFetch(channel, id) {
+    let oldmenu;
+    try {
+        oldmenu = await channel.messages.fetch(id);
+        return oldmenu;
+    } catch (error) {
+        console.log(error)
+        return undefined;
+    }
+}
+async function setBotMenuNotInService() {
+    //onsole.log("setBotMenuNotInService")
+    const channel = client.channels.cache.get(config.discord_setting.channelId);
+    let oldmenu = await getChannelMsgFetch(channel, botMenuId)
+    if (!oldmenu) return
+    //const embed = generateBotMenuEmbed();
+    const closeComponents = oldmenu.components.map(row => {
+        const newComponents = row.components.map(component => {
+            //console.log(component)
+            if (component.type === 'BUTTON') {
+                // If the component is a button, set it to be disabled
+                return component.setDisabled(true);
+            } else if (component.type === 'SELECT_MENU') {
+                return component.setDisabled(true).setPlaceholder('❌ | Not In Service');
+                // If the component is a selectmenu, set it to be disabled and clear its options
+            } else {
+                // If the component is not a button or selectmenu, just return it unmodified
+                return component;
+            }
+        });
+        // Return a new MessageActionRow with the modified components
+        return new MessageActionRow().addComponents(newComponents);
+    });
+    const author = {
+        name: "當前Bots",
+        iconURL: "https://i.imgur.com/AfFp7pu.png",
+        url: 'https://discord.js.org',
+    };
+    const embed = new MessageEmbed()
+        //.setDescription('Choose one of the following options:')
+        .setAuthor(author)
+        .setColor('RED')
+        .setThumbnail("https://i.imgur.com/AfFp7pu.png")
+        .addFields(
+            { name: `目前共 \`${0}\` 隻 bot`, value: '\`Not In Service\`' },
+        )
+        .setTimestamp()
+        .setFooter({ text: '關閉於', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+    await oldmenu.edit({ embeds: [embed], components: closeComponents });
+}
+function generateBotMenu() {
+    const embed = generateBotMenuEmbed();
+    let opts = []
+    for (let i = 0; i < bots.bots.length; i++) {
+        opts.push({
+            label: `${bots.name[i]}`,
+            // description: 'Open menu of Basic operations',
+            value: `botmenu-select-${bots.name[i]}`,
+        })
+    }
+    const row1 = new MessageActionRow().addComponents(
+        new MessageButton()
+            .setCustomId('botmenu-shift-btn')
+            .setLabel('下移')
+            .setStyle('SECONDARY'),
+        new MessageButton()
+            .setCustomId('botmenu-refresh-btn')
+            .setLabel('Refresh')
+            .setStyle('SUCCESS')
+            .setEmoji('♻️'),
+        new MessageButton()
+            .setCustomId('botmenu-close-btn')
+            .setLabel('Close')
+            .setStyle('DANGER')
+            .setEmoji('⚪')
+
+    );
+    const row2 = new MessageActionRow().addComponents(
+        new MessageSelectMenu()
+            .setCustomId('botmenu-select')
+            .setPlaceholder('Select a bot to operate')
+            .setMinValues(1)
+            .setMaxValues(1)
+            .addOptions(opts)
+    );
+    return { embeds: [embed], components: [row1, row2] }
+}
+function generateBotMenuEmbed() {
+    const author = {
+        name: "當前Bots",
+        iconURL: "https://i.imgur.com/AfFp7pu.png",
+        url: 'https://discord.js.org',
+    };
+    let botsfield = '';
+    const longestLength = bots.name.reduce((longest, a) => {
+        return a.length > longest ? a.length : longest;
+    }, 0);
+    for (let i = 0; i < bots.bots.length; i++) {
+        botsfield += (`${i})`.padEnd(parseInt(bots.bots.length / 10) + 2))
+        botsfield += (` ${bots.name[i]}`.padEnd(longestLength + 1))
+        botsfield += (` ${botstatus[bots.bots[i].status]}\n`)
+    }
+    const embed = new MessageEmbed()
+        //.setDescription('Choose one of the following options:')
+        .setAuthor(author)
+        .setColor('GREEN')
+        .setThumbnail("https://i.imgur.com/AfFp7pu.png")
+        .addFields(
+            { name: `目前共 \`${bots.name.length}\` 隻 bot`, value: '\`\`\`' + (botsfield ? botsfield : '無') + '\`\`\`' },
+        )
+        .setTimestamp()
+        .setFooter({ text: 'TEXXXTTTT', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+    return embed;
+}
+async function notImplementYet(interaction) {
+    await interaction.reply({
+        content: 'Not Implement yet',
+        ephemeral: true
     })
 }
 const exitcode = {
